f691228f0e559c608f1c5bf21678bc1ac80c6637
Shane Howearth <Shane.Howearth@shanehowearth.com>
2021-04-22 17:46:32 +1000

    Add 3 search functions (GetMax, GetMin, and Search(key))

--- trees/search/red_black/red_black.go
+++ trees/search/red_black/red_black.go
@@ -163,3 +163,58 @@ func (rt *Tree) Insert(
 	rt.Root.Colour = Black
 	return nil
 }
+
+// GetMax -
+func (rt *Tree) GetMax() (*Node, error) {
+	if rt.Root == nil {
+		return nil, fmt.Errorf("no nodes in the tree")
+	}
+	cmp := rt.Root
+	for {
+		if cmp.Child[Right] == nil {
+			return cmp, nil
+		}
+	}
+}
+
+// GetMin -
+func (rt *Tree) GetMin() (*Node, error) {
+	if rt.Root == nil {
+		return nil, fmt.Errorf("no nodes in the tree")
+	}
+	cmp := rt.Root
+	for {
+		if cmp.Child[Left] == nil {
+			return cmp, nil
+		}
+	}
+}
+
+// Search -
+func (rt *Tree) Search(key int) (*Node, error) {
+	if rt.Root == nil {
+		return nil, fmt.Errorf("no nodes in the tree")
+	}
+	cmp := rt.Root
+	for {
+		if key < cmp.Key {
+			if cmp.Child[Left] != nil {
+				cmp = cmp.Child[Left]
+				continue
+			} else {
+				return nil, fmt.Errorf("%d not found", key)
+			}
+		}
+		if key > cmp.Key {
+			if cmp.Child[Right] != nil {
+				cmp = cmp.Child[Right]
+				continue
+			} else {
+				return nil, fmt.Errorf("%d not found", key)
+			}
+		}
+		if key == cmp.Key {
+			return cmp, nil
+		}
+	}
+}
