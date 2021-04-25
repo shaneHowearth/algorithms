52ca231a8718d6e2f4e45333a165eaf0e51abac5
Shane Howearth <Shane.Howearth@shanehowearth.com>
2021-04-25 10:36:27 +1000

    Table tests for Search

--- trees/search/red_black/red_black_test.go
+++ trees/search/red_black/red_black_test.go
@@ -112,3 +112,64 @@ func TestInsert(t *testing.T) {
 		})
 	}
 }
+
+func TestSearch(t *testing.T) {
+	testcases := map[string]struct {
+		input  []int
+		key    int
+		output *redblack.Node
+		err    error
+	}{
+		"simple example exercises cmp.Child[Left]": {
+			input:  []int{7, 18, 3, 10, 22, 8, 11, 26},
+			key:    3,
+			output: &redblack.Node{Key: 3},
+		},
+		"simple example exercises cmp.Child[Right]": {
+			input:  []int{7, 18, 3, 10, 22, 8, 11, 26},
+			key:    22,
+			output: &redblack.Node{Key: 22},
+		},
+		"simple example exercises both possible paths": {
+			input:  []int{7, 18, 3, 10, 22, 8, 11, 26},
+			key:    11,
+			output: &redblack.Node{Key: 11},
+		},
+		"simple example exercises gets root": {
+			input:  []int{7, 18, 3, 10, 22, 8, 11, 26},
+			key:    7,
+			output: &redblack.Node{Key: 7},
+		},
+		"no root": {
+			err: fmt.Errorf("no nodes in the tree"),
+		},
+		"simple example with error key < cmp.Key": {
+			input: []int{7, 18, 3, 10, 22, 8, 11, 26},
+			key:   23,
+			err:   fmt.Errorf("%d not found", 23),
+		},
+		"simple example with error key > cmp.Key": {
+			input: []int{7, 18, 3, 10, 22, 8, 11, 26},
+			key:   27,
+			err:   fmt.Errorf("%d not found", 27),
+		},
+	}
+	for name, tc := range testcases {
+		t.Run(name, func(t *testing.T) {
+			rt := redblack.Tree{}
+			for i := range tc.input {
+				err := rt.Insert(tc.input[i], nil)
+				assert.Nil(t, err, "inserting %d generated error %v", tc.input[i], err)
+			}
+			node, err := rt.Search(tc.key)
+			if tc.err == nil {
+				assert.Nil(t, err, "got unexpected error searching for %d", tc.key)
+				assert.NotNil(t, node, "was expecting a node when searching for %d", tc.key)
+				assert.Equal(t, node.Key, tc.output.Key)
+			} else {
+				assert.NotNil(t, err, "was expecting an error searching for %d", tc.key)
+				assert.Nil(t, node, "was not expecting a node when searching for %d", tc.key)
+			}
+		})
+	}
+}
